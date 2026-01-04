c3a63350887d585c7583b1763593f214190ad7d5
DarkRoyal143 <153180564+DarkRoyal143@users.noreply.github.com>
2026-01-04 17:22:30 +0530

    Initialize Express server with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use('/api', require('./routes/api'));
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
